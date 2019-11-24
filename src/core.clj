f4446464dc6a903ee070a6b3fbced7c41a6a699e
Abhinav Sharma <abhi18av@gmail.com>
2019-11-24 18:31:01 +0530

    login code is done

--- src/core.clj
+++ src/core.clj
@@ -1,13 +1,33 @@
 (ns core
   (:use etaoin.api)
-  (:require [etaoin.keys :as keys]
-            [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]))
 
+(def secrets
+  (edn/read-string
+    (slurp "./src/secrets.edn")))
 
-(def driver (chrome {:path-driver  "/usr/local/bin/chromedriver"
-                       :path-browser "/Applications/Chromium.app/Contents/MacOS/Chromium"}))
+(def driver (firefox {:path-driver  "/usr/local/bin/geckodriver"
+                      :path-browser "/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox"}))
+
 
 ;;;;;;;;;
 
 
+(go driver "https://leetcode.com/accounts/login/")
+
+
+(defn login []
+  (click-el driver (first (query-all driver {:tag :input})))
+  (fill-active driver (:username secrets))
+  (wait 2)
+
+
+  (click-el driver (second (query-all driver {:tag :input})))
+  (fill-active driver (:password secrets))
+  (wait 2)
+
+  (click-el driver
+            (query driver {:fn/has-class "btn-content-container__214G"})))
+
+(login)
 
