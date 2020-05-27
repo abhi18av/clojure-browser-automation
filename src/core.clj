991403d9408230e070a045c787ba4b3c67a64bf8
Abhinav Sharma <abhi18av@gmail.com>
2020-05-27 21:21:25 +0530

    cleanup wrt the draftPost

--- src/core.clj
+++ src/core.clj
@@ -9,11 +9,6 @@
   (edn/read-string
     (slurp "./resources/secrets.edn")))
 
-(def draftPost
-  (edn/read-string
-    (slurp "./resources/draftPost.edn")))
-
-
 (def driver (firefox {:path-driver  (:path-driver secrets)
                       :path-browser (:path-browser secrets)}))
 
@@ -47,37 +42,6 @@
   (wait 2))
 
 
-
-(defn create-draft-post
-  "
-  Navigate the browser to the < Create message > page and uses the text provided in the
-  `./resources/draftPost.edn` to create a draft. Saves the screenshot of the page within the
-  `./resources` directory.
-
-  This function takes no argument.
-  "
-  []
-
-  (wait 3)
-
-  (click-el driver
-            (query driver {:css "#HeaderUserActions--NewPost"}))
-  (wait 3)
-
-  (click-el driver
-            (query driver {:css ".notranslate"}))
-  (wait 2)
-
-  (fill-active driver (:text draftPost))
-  (wait 2)
-
-  (click-el driver
-            (query driver {:fn/has-text "Save draft"}))
-  (wait 2)
-
-  (screenshot driver "./resources/draftPost.png")
-  (wait 2))
-
 ;;=========== Main code  =============
 (defn main
   "This function is the main entry point for the code.
@@ -85,8 +49,7 @@
   Just fire up the repl, move to this namespace and execute `(main)`."
   []
   (do
-    (login)
-    (create-draft-post)))
+    (login)))
 
 
 
